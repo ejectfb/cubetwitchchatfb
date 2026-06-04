ef28abdbe02bd026e146a819c49edb00909c9b3b
EjectFB <poma-9@yandex.ru>
2026-06-04 21:04:46 +0400

    Use screen events for 1.20.1 chat settings button

--- src/main/java/org/ejectfb/cubetwitchchatfb/TwitchCubeChatClient.java
+++ src/main/java/org/ejectfb/cubetwitchchatfb/TwitchCubeChatClient.java
@@ -1,7 +1,12 @@
 package org.ejectfb.cubetwitchchatfb;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.option.ChatOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
 
 public final class TwitchCubeChatClient implements ClientModInitializer {
     public static final String MOD_ID = "ejectfbcubetwitchchat";
@@ -13,7 +18,20 @@ public final class TwitchCubeChatClient implements ClientModInitializer {
     public void onInitializeClient() {
         config = TwitchConfig.load();
         chatClient = new TwitchIrcChatClient(MinecraftClient.getInstance());
+        registerSettingsButton();
         reloadConnection();
+    }
+
+    private static void registerSettingsButton() {
+        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (!(screen instanceof ChatOptionsScreen)) {
+                return;
+            }
+
+            Screens.getButtons(screen).add(ButtonWidget.builder(Text.literal("Twitch Chat"), button -> client.setScreen(new TwitchConfigScreen(screen)))
+                    .dimensions(scaledWidth / 2 - 100, scaledHeight - 56, 200, 20)
+                    .build());
+        });
     }
 
     public static TwitchConfig getConfig() {
